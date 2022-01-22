e036ab149ff8116803e96a1f3817bcf7e77a0787
Seung-hwan285 <ghks285@naver.com>
2022-01-22 18:03:13 +0900

    chore : 다시 테스트

---
+++ javascript-lotto/src/js/index.js
@@ -0,0 +1,18 @@
+const $=(s)=>document.querySelector(s);
+const showResultButton = $('.open-result-modal-button')
+const modalClose =$('.modal-close')
+const modal = $('.modal')
+
+
+const lottoNumbersToggleButton = $('.lotto-numbers-toggle-button')
+
+const onModalShow = () => {
+  modal.classList.add('open')
+}
+
+const onModalClose = () => {
+  modal.classList.remove('open')
+}
+
+showResultButton.addEventListener('click', onModalShow)
+modalClose.addEventListener('click', onModalClose)
